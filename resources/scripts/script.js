bc9c9fc3cb09b496f64391ef9d19f66095af44f8
Zugim <joe.reeve@gmail.com>
2024-11-06 22:06:00 +0900

    Edited to load level 0 by default

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,13 +1,43 @@
 const main = document.querySelector("main");
 const instruments = document.querySelectorAll(".instrument");
-const playButton = document.querySelector(".play");
 const submitButton = document.querySelector(".submit");
+const playButton = document.querySelector(".play");
 const guessesList = document.getElementById("guesses");
 
 let currLevel = 0;
 let currSelectedGuess = null;
 
 //utility functions (start)
+const generateGuessList = () => {
+  //clear previous level's list
+  guessesList.textContent = "";
+
+  //generate guess list items and add event listeners
+  for (let i = 0; i < currLevel + 4; i++) {
+    const guess = document.createElement("li");
+    guess.classList.add("guess");
+    guess.dataset.id = i;
+    guess.addEventListener("click", (el) => {
+      el.stopPropagation();
+      if (currSelectedGuess !== null) {
+        clearSelectedGuess();
+      }
+      el.target.classList.add("selected");
+      instruments.forEach((instrument) =>
+        instrument.classList.add("highlighted")
+      );
+      currSelectedGuess = el.target.dataset.id;
+    });
+    guessesList.appendChild(guess);
+  }
+  currLevel++;
+};
+
+const getSelectedGuess = () =>
+  [...document.querySelectorAll(".guess")].filter(
+    (guess) => guess.dataset.id === currSelectedGuess
+  )[0];
+
 const clearSelectedGuess = () =>
   getSelectedGuess().classList.remove("selected");
 
@@ -16,11 +46,13 @@ const clearHighlightedInstruments = () =>
     instrument.classList.remove("highlighted")
   );
 
-const getSelectedGuess = () =>
-  [...document.querySelectorAll(".guess")].filter(
-    (guess) => guess.dataset.id === currSelectedGuess
-  )[0];
+const toggleHiddenButton = () => {
+  submitButton.classList.toggle("hidden");
+  playButton.classList.toggle("hidden");
+};
 //utility functions (end)
+
+generateGuessList();
 
 //clear all selected and highlighted elements when main is clicked
 main.addEventListener("click", (el) => {
@@ -46,33 +78,8 @@ for (let i = 0; i < instruments.length; i++) {
 
 playButton.addEventListener("click", (el) => {
   el.stopPropagation();
-
-  //clear previous level's list
-  guessesList.textContent = "";
-
-  //generate guess list items and add event listeners
-  for (let i = 0; i < currLevel + 4; i++) {
-    const guess = document.createElement("li");
-    guess.classList.add("guess");
-    guess.dataset.id = i;
-    guess.addEventListener("click", (el) => {
-      el.stopPropagation();
-      if (currSelectedGuess !== null) {
-        clearSelectedGuess();
-      }
-      el.target.classList.add("selected");
-      instruments.forEach((instrument) =>
-        instrument.classList.add("highlighted")
-      );
-      currSelectedGuess = el.target.dataset.id;
-    });
-    guessesList.appendChild(guess);
-  }
-
-  el.target.classList.add("hidden");
-  submitButton.classList.remove("hidden");
-
-  currLevel++;
+  generateGuessList();
+  toggleHiddenButton();
 });
 
 submitButton.addEventListener("click", (el) => {
@@ -83,10 +90,9 @@ submitButton.addEventListener("click", (el) => {
       (guess) => guess.textContent === ""
     ).length === 0
   ) {
-    console.log("check answer");
-    el.target.classList.add("hidden");
-    playButton.classList.remove("hidden");
+    console.log("Checking answer...");
+    toggleHiddenButton();
   } else {
-    console.log("fill in your guess fully");
+    console.log("Please complete your guess");
   }
 });
